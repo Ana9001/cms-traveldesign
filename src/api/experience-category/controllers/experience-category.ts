1d46d6892fdf1d469d70d29cec0dcfd1f014e462
Jared Ortega <jortega@pagatodo.com>
2025-05-21 20:21:17 -0600

    first commit

---
+++ src/api/experience-category/controllers/experience-category.ts
@@ -0,0 +1,7 @@
+/**
+ * experience-category controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::experience-category.experience-category');
---
+++ src/api/experience-category/routes/experience-category.ts
@@ -0,0 +1,7 @@
+/**
+ * experience-category router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::experience-category.experience-category');
